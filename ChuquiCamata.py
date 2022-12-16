cf4acef57347bdeeb0d612a1ca692231d8a3e19b
Reynaldo Malave <contacto@reynaldomalave.com>
2022-12-16 19:45:13 -0300

    CambioCarpetaDestino

--- ChuquiCamata.py
+++ ChuquiCamata.py
@@ -2,10 +2,10 @@ import pandas as pd
 import requests
 import io
 
-ChuquiCamata = requests.post(r"https://webservice.hobolink.com/restv2/data/custom/file", json={"authentication":{"password": "Chuquicamata2022@","token": "z33VfGRP4k","user": "Chuquicamata"},"query": "ChuquiCamata"})
+ChuquiCamata = requests.post(r"https://webservice.hobolink.com/restv2/data/custom/file", json={"authentication":{"password": "Chuquicamatalixiviacion2022@","token": "z33VfGRP4k","user": "chuquicamatalixiviacion"},"query": "ChuquiCamata"})
 
 ChuquiCamata =  ChuquiCamata.text.encode('ISO-8859-1')
 
 ChuquiCamataPandas = pd.read_csv(io.StringIO( ChuquiCamata.decode('utf-8')), sep= "\t", index_col= 0, encoding= 'ISO-8859-1', low_memory= False)
 
-EscribirChuquiCamata = ChuquiCamataPandas.to_csv( r"C:\Users\rey\Desktop\Mtec\ChuquiCamata.csv" )
+EscribirChuquiCamata = ChuquiCamataPandas.to_csv( r"C:\Users\rey\OneDrive\MTEC\ChuquiCamata.csv" )
